2d88ad353fcf2f5aa931915892ab9647f4a14f99
LENOVO <LENOVO@LAPTOP-0I64B6UJ>
2023-01-13 00:01:51 +0530

    [nitish]:created sample2 class

---
+++ src/demo1/sample2.java
@@ -0,0 +1,13 @@
+package demo1;
+
+public class sample2
+{
+	public static void main(String[] args) 
+	{
+		String str="india";
+		
+		System.out.println("size of String str: "+str.length());
+		 
+	}
+	
+}
